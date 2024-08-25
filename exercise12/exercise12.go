f4d37c006b6065318bf8e4172e3e47d47502389b
Will Robertson <williamdrobertson42@gmail.com>
2024-08-25 11:28:44 +1000

    feat: display 2 places, round up

--- exercise12/exercise12.go
+++ exercise12/exercise12.go
@@ -5,6 +5,7 @@ import (
 	"exercises-in-programming/util"
 	"fmt"
 	"log"
+	"math"
 	"os"
 )
 
@@ -18,13 +19,13 @@ func Run() {
 		log.Fatalln(err)
 	}
 
-	rate, err := util.GetFloatWithPrompt(r, "Enter the rate of interest: ")
+	rawRate, err := util.GetFloatWithPrompt(r, "Enter the rate of interest: ")
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	rate = rate / 100
+	rate := rawRate / 100
 
 	years, err := util.GetIntWithPrompt(r, "Enter the number of years: ")
 
@@ -32,8 +33,9 @@ func Run() {
 		log.Fatalln(err)
 	}
 
-	total := util.FormatCents(int(float64(principal) * (1 + rate*float64(years))))
+	res := float64(principal) * (1 + rate*float64(years))
+	total := util.FormatCents(int(math.Round(res)))
 
-	fmt.Printf("After %d years at %f%%, the investment will be worth $%s\n", years, rate, total)
+	fmt.Printf("After %d years at %.1f%%, the investment will be worth $%s\n", years, rawRate, total)
 
 }
