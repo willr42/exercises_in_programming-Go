2e54593ecb7d013de7479aa72ff8424483077c37
Will Robertson <williamdrobertson42@gmail.com>
2024-08-25 20:40:58 +1000

    feat: ex15 challenge
    map of usernames/pw

--- exercise15/exercise15.go
+++ exercise15/exercise15.go
@@ -1,19 +1,47 @@
 package exercise15
 
 import (
+	"bufio"
+	"exercises-in-programming/util"
 	"fmt"
 	"log"
+	"os"
 	"syscall"
 
 	"golang.org/x/term"
 )
 
-const PASSWORD = "melon"
+type Creds struct {
+	user     string
+	password string
+}
+
+var Users = []Creds{{user: "alice", password: "wonderland"}, {user: "bob", password: "thebuilder"}}
 
 // Password validation
 func Run() {
 
-	fmt.Print("What is the password? ")
+	r := bufio.NewReader(os.Stdin)
+	user, err := util.GetInput(r, "What is your username? ")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	found := false
+	targetPass := ""
+
+	for _, v := range Users {
+		if user == v.user {
+			found = true
+			targetPass = v.password
+		}
+	}
+
+	if !found {
+		log.Fatalf("User %s not found.\n", user)
+	}
+
+	fmt.Print("What is your password? ")
 	bytepass, err := term.ReadPassword(syscall.Stdin)
 
 	if err != nil {
@@ -22,10 +50,10 @@ func Run() {
 
 	usrpass := string(bytepass)
 
-	if PASSWORD != usrpass {
-		fmt.Println("I don't know you.")
+	if targetPass != usrpass {
+		fmt.Println("\nI don't know you.")
 	} else {
-		fmt.Println("Welcome!")
+		fmt.Println("\nWelcome!")
 	}
 
 }
